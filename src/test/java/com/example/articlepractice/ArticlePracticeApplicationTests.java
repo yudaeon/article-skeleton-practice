4420e4705429a4a54c4493bf9d60591a876bb260
yudaeon <daeon03050@gmail.com>
2023-06-22 23:15:32 +0900

    article-skeleton 예제 commit

---
+++ src/test/java/com/example/articlepractice/ArticlePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.articlepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticlePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
